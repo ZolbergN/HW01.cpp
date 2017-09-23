9cae7d4033af8c9683006215a2817abd9e571384
Roman <exculibur@rambler.ru>
2017-09-24 01:42:34 +0300

    Update HW01.cpp

--- HW01.cpp
+++ HW01.cpp
@@ -12,6 +12,7 @@ private:
 	size_t array_size_;
 	size_t count_;
 public:
+	stack();
 	stack(int);
 	size_t count() const;
 	void push(T const &);
@@ -20,14 +21,27 @@ public:
 };
 
 template <typename T>
-stack<T>::stack(int maxEl) : array_size_(maxEl) {
+stack<T>::stack() {
+	array_size_ = 2;
 	array_ = new T[array_size_];
+}
+
+template <typename T>
+stack<T>::stack(int maxEl) : array_size_(maxEl) {
+	array_ = new T[maxEl];
 	count_ = 0;
 }
 
 template <typename T>
 void stack<T>::push(const T &value) {
-	assert(count_ < array_size_);
+	if (count_ == array_size_) {
+		T* array_size2_ = new T[2 * array_size_];
+		for (int i = 0; i != array_size_; ++i) {
+			array_size2_[i] = array_[i];
+		}
+		delete[]array_;
+		array_ = array_size2_;
+	}
 	array_[count_++] = value;
 }
 
@@ -35,6 +49,7 @@ template<typename T>
 T stack<T>::pop() {
 	assert(count_ > 0);
 	array_[--count_];
+	return array_[count_];
 }
 
 template<typename T>
@@ -49,19 +64,31 @@ void stack<T>::printStack() {
 }
 
 int main() {
-	stack<int> st(5);
+	stack<int> st(2);
 	int j = 0;
 	int je;
 
-	while (j++ < 5) {
+	while (j++ < 4) {
 		cin >> je;
 		st.push(je);
 	}
 
-	cout << endl;
+cout << "Ellements of stack" << endl;
+cout << endl;
 
-	st.printStack();
+st.printStack();
 
-	system("pause");
-	return 0;
+for (int i = 4; i > 1; i--) {
+	st.pop();
+}
+
+cout << "Posle Ydaleniya" << endl;
+cout << endl;
+
+st.printStack();
+
+cout << endl;
+
+system("pause");
+return 0;
 }
